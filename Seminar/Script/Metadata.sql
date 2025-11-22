3319616c8e323c6ba8572c8534b2a5a31da9dc51
danhuy0101 <tdhuy22@clc.fitus.edu.vn>
2025-11-22 21:44:27 +0700

    Add seminar

---
+++ Seminar/Script/Metadata.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE DATABASE [MetaData_Sales]
+GO
+USE [MetaData_Sales]
+GO
+
+CREATE TABLE [dbo].[ETL_Load_Control](
+	[id] [int] NOT NULL PRIMARY KEY,
+	[name] [nchar](50) NOT NULL,
+	[LSET] [datetime] NULL, -- Last Successful Extraction Time (Dùng để lọc bản ghi)
+	[CET] [datetime] NULL  -- Current Extraction Time (Dùng để ghi thời điểm bắt đầu chạy)
+) ON [PRIMARY]
+GO
+
+
+INSERT INTO [dbo].[ETL_Load_Control] ([id], [name], [LSET], [CET])
+VALUES 
+    (1, N'DimCustomer_Load', '1900-01-01 00:00:00.000', '1900-01-01 00:00:00.000'),
+    (2, N'DimProduct_Load', '1900-01-01 00:00:00.000', '1900-01-01 00:00:00.000'),
+    (3, N'FactSales_Load', '1900-01-01 00:00:00.000', '1900-01-01 00:00:00.000'); 
+GO
---
+++ Seminar/Script/Stage.sql
@@ -0,0 +1,62 @@
+﻿
+CREATE DATABASE StagingDB_Seminar
+GO
+USE StagingDB_Seminar;
+GO
+
+CREATE TABLE dbo.Customer_Stage (
+    CustomerID NVARCHAR(50) PRIMARY KEY, 
+    CustomerName NVARCHAR(255),
+    Segment NVARCHAR(50),
+    Country NVARCHAR(50),
+    City NVARCHAR(50),
+    StateProvince NVARCHAR(50),
+    
+    CreateDate DATETIME DEFAULT GETDATE(),
+    UpdateDate DATETIME NULL
+);
+GO
+
+
+CREATE TABLE dbo.Product_Stage (
+    ProductID NVARCHAR(50) PRIMARY KEY,
+    Category NVARCHAR(50),
+    SubCategory NVARCHAR(50),
+    ProductName NVARCHAR(255),
+    
+    CreateDate DATETIME DEFAULT GETDATE(),
+    UpdateDate DATETIME NULL
+);
+GO
+
+CREATE TABLE dbo.Sales_Combined_Stage (
+    OrderID NVARCHAR(50) PRIMARY KEY, 
+    
+    OrderDate DATETIME,
+    CustomerID NVARCHAR(50),
+    ProductID NVARCHAR(50),
+    Sales DECIMAL(18, 2),
+    Quantity INT,
+    Profit DECIMAL(18, 4), 
+
+    SourceID INT NOT NULL, 
+    CreateDate DATETIME DEFAULT GETDATE(),
+    UpdateDate DATETIME NULL
+);
+GO
+
+-- 1. Thêm Khóa Ngoại cho Customer ID
+ALTER TABLE dbo.Sales_Combined_Stage
+ADD CONSTRAINT FK_Sales_Customer
+FOREIGN KEY (CustomerID)
+REFERENCES dbo.Customer_Stage (CustomerID);
+GO
+
+-- 2. Thêm Khóa Ngoại cho Product ID
+ALTER TABLE dbo.Sales_Combined_Stage
+ADD CONSTRAINT FK_Sales_Product
+FOREIGN KEY (ProductID)
+REFERENCES dbo.Product_Stage (ProductID);
+GO
+
+
